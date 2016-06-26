3ab99dcba53329be1ee14229774b982df4835b88
AntikCz <antik494@gmail.com>
2016-06-26 14:00:39 +0200

    Added soft extensions

---
+++ src/DI/SoftExtensionsExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Thunbol\DI;
+
+use Nette\DI\CompilerExtension;
+use Nette\DI\Statement;
+
+class SoftExtensionsExtension extends CompilerExtension {
+
+	public function loadConfiguration() {
+		$extensions = $this->compiler->getExtensions();
+		$prepare = [];
+		foreach ($this->getConfig() as $name => $class) {
+			if (isset($extensions[$name])) {
+				continue;
+			}
+			if ($class === FALSE) {
+				if (isset($prepare[$name])) {
+					unset($prepare[$name]);
+				}
+				continue;
+			}
+			if ($class instanceof Statement) {
+				if (class_exists($class->getEntity())) {
+					$rc = new \ReflectionClass($class->getEntity());
+					$prepare[$name] = $rc->newInstanceArgs($class->arguments);
+				}
+			} else {
+				if (class_exists($class)) {
+					$prepare[$name] = new $class;
+				}
+			}
+		}
+
+		foreach ($prepare as $name => $class) {
+			$this->compiler->addExtension($name, $class);
+		}
+	}
+
+}
